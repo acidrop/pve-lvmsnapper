c023b6fe70ee10f0d45399c3264c8a423501c8f4
acidrop <yannis.milios@gmail.com>
2018-09-07 10:40:09 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple bash script for automatic snapshot of LVM Thin volumes (VM) on Proxmox VE.
---
+++ pve-lvmsnapper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+KEEP_DAYS=3
+VG="vg_drbd" # LVM volume group we are snapshoting
+BACKUP_PREFIX="snap-" # Prefix of snapshot volume name.
+
+
+# Create new snapshot
+TODAY="$(date +%F)" 
+    lvs | awk '{print $1}' | grep -v "snap" | grep -v "thinpool" | grep -v "LV" > /tmp/001 && for LV in $(cat /tmp/001);do /sbin/lvcreate -s --name "$BACKUP_PREFIX$LV-$TODAY"  "$VG/$LV";done
+
+
+# Clean old snapshots.
+lvs -o lv_name --noheadings | sed -n "s@$BACKUP_PREFIX@@p" | while read DATE; do
+    TS_DATE=$(date -d "$DATE" +%s)
+    TS_NOW=$(date +%s)
+    AGE=$(( (TS_NOW - TS_DATE) / 86400))
+    if [ "$AGE" -ge "$KEEP_DAYS" ]; then
+        VOLNAME="$BACKUP_PREFIX$LV-$DATE" 
+        /sbin/lvremove -f "$VG/$VOLNAME" 
+    fi
+done
+
